8a50044951256785dbf9ef3cdcaa2b839d50c8e1
ronishamai <ronishamai@mail.tau.ac.il>
2023-07-22 18:49:32 +0300

    List Views improvements

--- SDSApplication/SDSApplication/models.cs
+++ SDSApplication/SDSApplication/models.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Maui.Controls.Maps;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
