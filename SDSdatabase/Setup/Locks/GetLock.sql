ff4770888fc3fe0cf38d6b5933710a909956b5bb
Alon Bebchuk <alon.bebchuk@gmail.com>
2023-08-13 18:39:31 +0300

    pricing

--- SDSdatabase/Setup/Locks/GetLock.sql
+++ SDSdatabase/Setup/Locks/GetLock.sql
@@ -13,12 +13,31 @@ RETURNS @Locks Table
     lock_name NVARCHAR(MAX) NOT NULL,
     -- Rental Data
     hourly_rate DECIMAL(4,2) NOT NULL,
-    start_time DATETIME
+    start_time DATETIME,
+    end_time DATETIME,
+    duration_days INT,
+    duration_hours INT,
+    cost AS CONVERT(DECIMAL(9,2), hourly_rate) * (24 * duration_days + duration_hours)
 )
 BEGIN
     DECLARE @lockStatus INT;
     SELECT @lockStatus = [dbo].[GetLockStatus](@user_id, @lock_id);
 
+    DECLARE @now DATETIME;
+    SET @now = GETDATE();
+
+    WITH
+    LockHourDifference
+    AS
+    (
+        SELECT
+            *,
+            IIF(@lockStatus = 1, DATEDIFF(MINUTE, start_time, @now) / 60, NULL) AS hour_difference
+        FROM
+            Locks
+        WHERE
+            id = @lock_id
+    )
     INSERT INTO @Locks
     SELECT
         @lockStatus,
@@ -31,11 +50,12 @@ BEGIN
         name,
         -- Rental Data
         IIF(@lockStatus = 0, station_hourly_rate, hourly_rate),
-        start_time
+        start_time,
+        IIF(@lockStatus = 1, @now, NULL),
+        IIF(@lockStatus = 1, hour_difference / 24, NULL),
+        IIF(@lockStatus = 1, hour_difference % 24, NULL)
     FROM
-        Locks
-    WHERE
-        id = @lock_id;
+        LockHourDifference;
 
     RETURN;
 END;
